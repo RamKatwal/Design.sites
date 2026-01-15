df704989509cfb25f7b210377b69c31d42bee89d
Ram Katwal <ramkatwal8315@gmail.com>
2026-01-15 09:24:04 +0545

    build error fixed

--- src/app/sites/[slug]/page.tsx
+++ src/app/sites/[slug]/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
 import { Header } from '@/features/common/Header';
-import { getSiteBySlug } from '@/features/sites/lib/getSites';
+import { getSiteBySlug } from '@/features/lib/getSites';
 import { sites } from '@/data/sites';
 
 // Generate static params for all known sites to optimize build
